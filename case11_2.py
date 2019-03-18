8196f0ba920c08cbffa08ceb83bae1f5e8a4921c
Кирилл Паймушкин (Kirill Paymushkin) <44719713+paymtime@users.noreply.github.com>
2019-03-18 16:55:51 +0700

    Update case11_2.py

--- case11_2.py
+++ case11_2.py
@@ -21,3 +21,32 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+def ice(order, size):
+    koch(order, size)
+    turtle.right(120)
+    koch(order, size)
+    turtle.right(120)
+    koch(order, size)
+
+def koch(order, size):
+    if order == 0:          
+        turtle.forward(size)
+    else:
+        koch(order-1, size/3)   
+        turtle.left(60)
+        koch(order-1, size/3)
+        turtle.right(120)
+        koch(order-1, size/3)
+        turtle.left(60)
+        koch(order-1, size/3)
+
+def main1():
+    turtle.up()
+    turtle.goto(-100,0)
+    turtle.down()
+    n = int(input('Глубина рекурсии:'))
+    a = int(input('Длина стороны:'))
+    ice(n, a)
+
+main1()
